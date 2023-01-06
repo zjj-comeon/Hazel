5bb76a045140addbc5c61181e611e971f0dc3496
zjj-comeon <1753524606@qq.com>
2023-01-06 12:06:27 +0800

    initial commit

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,12 @@
+#include "Test.h"
+
+#include <stdio.h>
+
+namespace Hazel {
+
+	void Print()
+	{
+		printf("Welcome to Hazel Engine!");
+	}
+
+}
---
+++ SandBox/src/Application.cpp
@@ -0,0 +1,8 @@
+namespace Hazel {
+	__declspec(dllimport) void Print();
+}
+
+int main()
+{
+	Hazel::Print();
+}
