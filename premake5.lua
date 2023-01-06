378df755e92455ddc7694fe25cd73296f74ca3e4
zjj-comeon <1753524606@qq.com>
2023-01-06 14:28:14 +0800

    add premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Hazel"
+    architecture "x64"
+    startproject "SandBox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"    
+
+project "Hazel"
+    location "Hazel"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {   
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "10.0"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPYDIR} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SandBox")
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+
+project "SandBox"
+    location "SandBox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Hazel/src",
+        "Hazel/vendor/spdlog/include"
+    }
+
+    links 
+    {
+        "Hazel"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "10.0"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
