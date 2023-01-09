5e2a1ad835515401b16d7c97061356d0d1cce9f6
zjj-comeon <1753524606@qq.com>
2023-01-09 17:21:51 +0800

    add glm math library

--- premake5.lua
+++ premake5.lua
@@ -13,9 +13,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
-IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
-IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
+IncludeDir["GLFW"]  = "Hazel/vendor/GLFW/include"
+IncludeDir["Glad"]  = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
+IncludeDir["glm"]   = "Hazel/vendor/glm"
 
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
@@ -36,7 +37,9 @@ project "Hazel"
     files
     {   
         "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/glm/glm/**.hpp",
+        "%{prj.name}/vendor/glm/glm/**.inl",
     }
 
     includedirs
@@ -45,7 +48,8 @@ project "Hazel"
         "%{prj.name}/src",
         "%{IncludeDir.GLFW}",
         "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}"
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.glm}"
     }
 
     links
@@ -105,7 +109,8 @@ project "SandBox"
     includedirs
     {
         "Hazel/src",
-        "Hazel/vendor/spdlog/include"
+        "Hazel/vendor/spdlog/include",
+        "%{IncludeDir.glm}"
     }
 
     links 
